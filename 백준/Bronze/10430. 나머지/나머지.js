95f09012fd4c6f621944ed51d54f7c190de9420c
김민경 <162336698+m01310g@users.noreply.github.com>
2025-01-12 19:35:33 +0900

    [Bronze V] Title: 나머지, Time: 116 ms, Memory: 9744 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,20 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.on("line", (line) => {
+    input = line;
+    rl.close();
+});
+
+rl.on("close", () => {
+    const [a, b, c] = input.split(" ").map(Number);
+
+    console.log((a + b) % c);
+    console.log(((a % c) + (b % c)) % c);
+    console.log((a * b) % c);
+    console.log(((a % c) * (b % c)) % c);
+});
