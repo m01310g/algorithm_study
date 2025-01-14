b3f2fd716c111714249ea3d073531c309d993f81
김민경 <162336698+m01310g@users.noreply.github.com>
2025-01-14 21:12:22 +0900

    [level 1] Title: 폰켓몬, Time: 0.57 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,5 +1,8 @@
 function solution(nums) {
-    const uniqNum = new Set(nums)
-    const answer = Math.min(uniqNum.size, nums.length/2)
-    return answer;
+    const uniqNum = new Set(nums);
+    if (uniqNum.size <= nums.length / 2) {
+        return uniqNum.size;
+    } else {
+        return nums.length / 2;
+    }
 }
