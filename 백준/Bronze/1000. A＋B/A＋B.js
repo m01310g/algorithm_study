d15e92aff4a527caf0d493dc3ed0087ebdc82077
김민경 <162336698+m01310g@users.noreply.github.com>
2025-04-12 15:58:29 +0900

    [Bronze V] Title: A+B, Time: 96 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.js
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -1,7 +1,3 @@
-const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().split(" ");
+const input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
 
-const a = parseInt(input[0]);
-const b = parseInt(input[1]);
-
-console.log(a + b);
+console.log(Number(input[0]) + Number(input[1]));
