d8aa37db95664096a3cf57fdba228dceb5eef6fb
김민경 <162336698+m01310g@users.noreply.github.com>
2025-04-12 16:05:08 +0900

    [Bronze V] Title: ??!, Time: 116 ms, Memory: 9472 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.js
+++ 백준/Bronze/10926. ？？！/？？！.js
@@ -1,4 +1,14 @@
-const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim();
+const readline = require("readline");
 
-console.log(`${input}??!`);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (line) => {
+  const input = line.trim();
+
+  console.log(input + "??!");
+
+  rl.close();
+});
