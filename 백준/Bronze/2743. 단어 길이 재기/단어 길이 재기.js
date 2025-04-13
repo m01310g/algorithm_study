bf766f32bf660a5ec70f41e9e85c371cb70bd46a
김민경 <162336698+m01310g@users.noreply.github.com>
2025-04-14 00:03:55 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 104 ms, Memory: 9452 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.js
@@ -0,0 +1,15 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const input = [];
+rl.on("line", (line) => {
+  input.push(line.trim().toString());
+  rl.close();
+}).on("close", () => {
+  console.log(input[0].length);
+
+  process.exit();
+});
