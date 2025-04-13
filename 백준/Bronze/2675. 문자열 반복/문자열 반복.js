81799ef8a2c9519283a498808b7b6c3eb44cb1e3
김민경 <162336698+m01310g@users.noreply.github.com>
2025-04-14 01:35:08 +0900

    [Bronze II] Title: 문자열 반복, Time: 100 ms, Memory: 9460 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.js
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.js
@@ -1,36 +1,31 @@
-const { join } = require("path");
 const readline = require("readline");
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
-let input = [];
-let count = 0;
-
+const input = [];
 rl.on("line", (line) => {
-    count++;
-    if (count === 1) {
-        input.push(Number(line.trim()));
-    } else {
-        input.push(line.split(" "));
+  input.push(line.trim());
+
+  if (input.length === Number(input[0]) + 1) rl.close();
+}).on("close", () => {
+  const t = Number(input[0]);
+  const ans = [];
+
+  for (let i = 1; i < t + 1; i++) {
+    const [r, s] = input[i].split(" ");
+
+    let result = "";
+    for (char of s) {
+      result += char.repeat(Number(r));
     }
 
-    if (input.length - 1 === input[0]) {
-        rl.close();
-    }
-});
+    ans.push(result);
+    result = "";
+  }
 
-rl.on("close", () => {
-    const num = input[0];
-    for (let i = 1; i <= num; i++) {
-        const count = input[i][0];
-        const word = input[i][1];
-        let result = "";
-        for (let j = 0; j < word.length; j++) {
-            result += word[j].repeat(count);
-        }
-        console.log(result);
-    }
+  console.log(ans.join("\n"));
+  process.exit();
 });
