0cab3394ca35df62657e0c22ea856b604e487ea7
김민경 <162336698+m01310g@users.noreply.github.com>
2025-04-13 23:11:00 +0900

    [Bronze II] Title: 공 바꾸기, Time: 116 ms, Memory: 9496 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.js
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.js
@@ -1,37 +1,28 @@
 const readline = require("readline");
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
-let input = [];
-let count = 0;
-
+const input = [];
 rl.on("line", (line) => {
-    count++;
-    if (count === 1) {
-        input.push(line.split(" ").map(Number));
-    } else {
-        input.push(line.split(" ").map(Number));
-    }
+  input.push(line.trim().split(" ").map(Number));
+  if (input.length === input[0][1] + 1) rl.close();
+}).on("close", () => {
+  const [n, m] = input[0];
+  const result = Array.from({ length: n }, (_, index) => index + 1);
 
-    if (count === input[0][1] + 1) {
-        rl.close();
-    }
-});
+  for (let x = 1; x < m + 1; x++) {
+    const [i, j] = input[x];
+    let tmp;
 
-rl.on("close", () => {
-    const [n, m] = input[0];
-    const result = Array.from({ length: n }, (_, index) => index + 1);
+    tmp = result[i - 1];
+    result[i - 1] = result[j - 1];
+    result[j - 1] = tmp;
+  }
 
-    for (let i = 1; i <= m; i++) {
-        let temp;
-        const [x, y] = input[i];
-        temp = result[x - 1];
-        result[x - 1] = result[y - 1];
-        result[y - 1] = temp;
-    }
+  console.log(result.join(" "));
 
-    console.log(result.join(" "));
+  process.exit();
 });
