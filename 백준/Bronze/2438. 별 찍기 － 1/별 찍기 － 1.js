297fb655606552e6d988a20a6530e3018df3db18
김민경 <162336698+m01310g@users.noreply.github.com>
2025-04-13 21:13:28 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 104 ms, Memory: 9488 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -0,0 +1,17 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (line) => {
+  const input = Number(line.trim());
+  const stars = [];
+
+  for (let i = 0; i < input; i++) {
+    stars.push("*".repeat(i + 1));
+  }
+  console.log(stars.join("\n"));
+  rl.close();
+});
