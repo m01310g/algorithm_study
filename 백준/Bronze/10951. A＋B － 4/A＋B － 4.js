aa77acad09197111246a044bdbbb2c4c5a5ed222
김민경 <162336698+m01310g@users.noreply.github.com>
2025-04-13 21:39:58 +0900

    [Bronze V] Title: A+B - 4, Time: 112 ms, Memory: 9760 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,19 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const result = [];
+
+rl.on("line", (line) => {
+  const [a, b] = line.trim().split(" ").map(Number);
+
+  if (a === 0 && b === 0) {
+    console.log(result.join("\n"));
+    rl.close();
+  } else {
+    console.log(a + b);
+  }
+});
