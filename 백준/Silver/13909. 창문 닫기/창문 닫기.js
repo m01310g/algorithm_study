0b35909290e95c6efcdd03c9313d40651c1f28e3
김민경 <162336698+m01310g@users.noreply.github.com>
2025-05-05 00:40:21 +0900

    [Silver V] Title: 창문 닫기, Time: 100 ms, Memory: 9692 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.js
@@ -0,0 +1,17 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+rl.on("line", (line) => {
+  input.push(Number(line.trim()));
+  rl.close();
+}).on("close", () => {
+  const num = input[0];
+  console.log(Math.floor(Math.sqrt(num)));
+
+  process.exit();
+});
