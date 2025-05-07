0cbf47ffcd9924de664391f95e626b0e9782fc42
김민경 <162336698+m01310g@users.noreply.github.com>
2025-05-07 15:30:46 +0900

    [Bronze IV] Title: 녹색거탑, Time: 104 ms, Memory: 9472 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.js
@@ -0,0 +1,15 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+rl.on("line", (line) => {
+  input.push(Number(line.trim()));
+  rl.close();
+}).on("close", () => {
+  const [n] = input;
+  console.log(Math.pow(2, n));
+});
