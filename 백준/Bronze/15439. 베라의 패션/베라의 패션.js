87aedaa68ec139031ee84ed7c9a5d18c40c4a123
김민경 <162336698+m01310g@users.noreply.github.com>
2025-05-07 15:28:49 +0900

    [Bronze IV] Title: 베라의 패션, Time: 108 ms, Memory: 9452 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.js
@@ -0,0 +1,16 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+rl.on("line", (line) => {
+  input.push(Number(line.trim()));
+  rl.close();
+}).on("close", () => {
+  const [n] = input;
+  console.log(n * (n - 1));
+  process.exit();
+});
