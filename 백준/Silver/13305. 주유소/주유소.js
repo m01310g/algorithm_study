fec6ebd9e91f15c82adfeb0eb2e8dfebd8cd24c6
김민경 <162336698+m01310g@users.noreply.github.com>
2025-06-08 15:32:08 +0900

    [Silver III] Title: 주유소, Time: 304 ms, Memory: 45524 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -7,17 +7,17 @@ const rl = readline.createInterface({
 
 const input = [];
 rl.on("line", (line) => {
-  input.push(line.trim().split(" ").map(Number));
+  input.push(line.trim().split(" ").map(BigInt));
   if (input.length === 3) rl.close();
 }).on("close", () => {
   const n = input[0][0];
   const dist = input[1];
   const price = input[2];
 
-  let total = 0;
+  let total = 0n;
   let minPrice = price[0];
 
-  for (let i = 0; i < n - 1; i++) {
+  for (let i = 0; i < n - 1n; i++) {
     if (price[i] < minPrice) {
       minPrice = price[i];
     }
@@ -25,7 +25,7 @@ rl.on("line", (line) => {
     total += minPrice * dist[i];
   }
 
-  console.log(total);
+  console.log(total.toString());
 
   process.exit();
 });
