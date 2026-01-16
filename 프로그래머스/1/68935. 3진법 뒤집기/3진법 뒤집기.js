3ac1d87ab03eebadd239af611d99aaa580672d81
김민경 <162336698+m01310g@users.noreply.github.com>
2026-01-16 17:02:06 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,5 +1,6 @@
 function solution(n) {
-    const n3 = n.toString(3);
-    const reversed = n3.split("").reverse().join("");
-    return parseInt(reversed, 3);
+    var answer = 0;
+    const num = n.toString(3).split("").reverse().join("");
+    answer = parseInt(num, 3);
+    return answer;
 }
