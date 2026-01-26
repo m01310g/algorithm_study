5ac42c06216e03ae5b3d2cfd6137ff276a3d5af5
김민경 <162336698+m01310g@users.noreply.github.com>
2026-01-26 16:59:40 +0900

    [level 2] Title: 타겟 넘버, Time: 15.78 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,22 +1,19 @@
 function solution(numbers, target) {
+    let answer = 0;
     let count = 0;
     
-    const dfs = (index, sum) => {
-        // 배열의 끝까지 도달했을 때
+    const DFS = (index, sum) => {
         if (index === numbers.length) {
-            if (sum === target) {
-                // 카운팅 로직
-                count++;
-            }
+            if (sum === target) count++;
             return;
         }
-        // 현재 숫자를 더하는 경우
-        dfs(index + 1, sum + numbers[index]);
-        // 현재 숫자를 빼는 경우
-        dfs(index + 1, sum - numbers[index]);
+        
+        DFS(index + 1, sum + numbers[index]);
+        DFS(index + 1, sum - numbers[index]);
     }
     
-    dfs(0, 0);
+    DFS(0, 0);
     
-    return count;
+    answer = count;
+    return answer;
 }
