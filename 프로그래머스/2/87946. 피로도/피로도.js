76e06f739ca312609014f2f6f99d163398f8fab5
김민경 <162336698+m01310g@users.noreply.github.com>
2026-01-26 17:11:16 +0900

    [level 2] Title: 피로도, Time: 424.25 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -2,8 +2,8 @@ function solution(k, dungeons) {
     let answer = 0;
     const visited = Array(dungeons.length).fill(false);
     
-    const DFS = (k, num) => {
-        answer = Math.max(answer, num);
+    const DFS = (k, num) => {   // 현재 피로도, 방문 횟수
+        answer = Math.max(num, answer);
         
         for (let i = 0; i < dungeons.length; i++) {
             if (!visited[i] && k >= dungeons[i][0]) {
@@ -15,6 +15,6 @@ function solution(k, dungeons) {
     }
     
     DFS(k, 0);
-
+    
     return answer;
 }
