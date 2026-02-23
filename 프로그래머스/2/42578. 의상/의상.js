93121b650a4726a20fc984659353c490a360d9b9
김민경 <162336698+m01310g@users.noreply.github.com>
2026-02-23 18:36:41 +0900

    [level 2] Title: 의상, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,14 +1,14 @@
 function solution(clothes) {
-    let answer = 1;
+    var answer = 1;
     const clothMap = {};
     
     for (const c of clothes) {
         const [name, type] = c;
-        clothMap[type] = (clothMap[type] || 0) + 1
+        clothMap[type] = (clothMap[type] || 0) + 1;
     }
     
     for (const type in clothMap) {
-        answer *= (clothMap[type] + 1)
+        answer *= clothMap[type] + 1
     }
     
     return answer - 1;
