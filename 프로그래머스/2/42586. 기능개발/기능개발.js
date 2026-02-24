d85be40cb4a9f5333caaaff7378cfcb660f3ab69
김민경 <162336698+m01310g@users.noreply.github.com>
2026-02-24 19:03:12 +0900

    [level 2] Title: 기능개발, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,26 +1,19 @@
 function solution(progresses, speeds) {
     var answer = [];
-    const completed = [];
+    const completed = progresses.map((p, i) => Math.ceil((100 - p) / speeds[i]));   // 각각의 완료일 계산
     
-    for (let i = 0; i < progresses.length; i++) {
-        const day = Math.ceil((100 - progresses[i]) / speeds[i]);
-        completed.push(day);
-    }
-
-    let current = completed[0];
-    let count = 1;
+    let curr = completed[0];
+    let cnt = 1;
     
     for (let i = 1; i < completed.length; i++) {
-        if (completed[i] <= current) {
-            count++;
-        } else {
-            answer.push(count);
-            current = completed[i];
-            count = 1;
+        if (completed[i] <= curr) cnt++;    // 기준일보다 작거나 같으면 cnt + 1 
+        else {  // 기준일보다 크면 push, 기준일 갱신
+            answer.push(cnt);
+            curr = completed[i];
+            cnt = 1;
         }
     }
-    
-    answer.push(count);
+    answer.push(cnt);
     
     return answer;
 }
