82ea1d3b89f0ad6f75c65dfe3d85ddfc2780b325
김민경 <162336698+m01310g@users.noreply.github.com>
2026-02-26 22:26:30 +0900

    [level 3] Title: 네트워크, Time: 0.58 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -3,10 +3,10 @@ function solution(n, computers) {
     const visited = Array(n).fill(false);
     let count = 0;
     
-    function dfs(node) {
-    visited[node] = true;
-    for (let i = 0; i < n; i++) {
-        if (i !== node && computers[node][i] === 1 && !visited[i]) dfs(i, visited);
+    const dfs = (node) => {
+        visited[node] = true;
+        for (let i = 0; i < n; i++) {
+            if (i !== node && computers[node][i] === 1 && !visited[i]) dfs(i);
         }
     }
     
