65fd4d2527fd150ecfec83c4f0b2ed3adc58a3ef
김민경 <162336698+m01310g@users.noreply.github.com>
2026-02-26 23:49:14 +0900

    [level 3] Title: 단어 변환, Time: 0.29 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,30 +1,30 @@
-function canTransform(word1, word2) {
-    let diff = 0;
-    for (let i = 0; i < word1.length; i++) {
-        if (word1[i] !== word2[i]) diff++;
-        if (diff > 1) return false;
-    }
-    
-    return diff === 1;
-}
-
 function solution(begin, target, words) {
     if (!words.includes(target)) return 0;
     
+    const queue = [];
     let count = 0;
-    const queue = [[begin, count]];
-    
+    let head = 0;
     const visited = Array(words.length).fill(false);
     
-    while (queue.length) {
-        const [current, steps] = queue.shift();
-        
-        if (current === target) return steps;
+    queue.push([begin, count]);
+
+    const compare = (word1, word2) => {
+        let diff = 0;
+        for (let i = 0; i < word1.length; i++) {
+            if (word1[i] !== word2[i]) diff++;
+            if (diff > 1) return false;
+        }
+        return diff === 1;
+    }
+    
+    while (head < queue.length) {
+        const [curr, step] = queue[head++];
+        if (curr === target) return step;
         
         for (let i = 0; i < words.length; i++) {
-            if (!visited[i] && canTransform(current, words[i])) {
+            if (!visited[i] && compare(curr, words[i])) {
                 visited[i] = true;
-                queue.push([words[i], steps + 1]);
+                queue.push([words[i], step + 1]);
             }
         }
     }
