c90664d02cad192d0e1f3bddf088e8d435cec060
김민경 <162336698+m01310g@users.noreply.github.com>
2026-03-05 22:07:59 +0900

    [level 3] Title: 정수 삼각형, Time: 4.70 ms, Memory: 41.3 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,15 +1,14 @@
 function solution(triangle) {
-    let answer = 0;
-    const n = triangle.length;
+    var answer = 0;
     const dp = triangle;
+    const n = triangle.length;
     
     for (let i = n - 2; i >= 0; i--) {
         for (let j = 0; j < triangle[i].length; j++) {
-          dp[i][j] += Math.max(dp[i + 1][j], dp[i + 1][j + 1]);  
+            dp[i][j] += Math.max(dp[i + 1][j], dp[i + 1][j + 1])
         }
     }
     
     answer = dp[0][0];
-    
     return answer;
 }
