aa1bef9169336960af6929f8d7eb4f8911469c43
김민경 <162336698+m01310g@users.noreply.github.com>
2026-03-08 20:42:21 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 58.50 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.js
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.js
@@ -1,22 +1,23 @@
-function solution(board)
-{
-    const dp = Array.from(Array(board.length), () => new Array(board[0].length).fill(0));
-    let answer = 0;
+function solution(board) {
+    const n = board.length;
+    const m = board[0].length;
+    const dp = Array.from(Array(n), () => Array(m).fill(0));
     let maxLen = 0;
+    let answer = 0;
     
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
             if (board[i][j] === 0) {
                 dp[i][j] = 0;
                 continue;
-            } 
+            }
             
             if (i === 0 || j === 0) {
                 dp[i][j] = 1;
             } else {
                 dp[i][j] = Math.min(
-                    dp[i - 1][j], 
-                    dp[i][j - 1], 
+                    dp[i - 1][j],
+                    dp[i][j - 1],
                     dp[i - 1][j - 1]
                 ) + 1;
             }
@@ -26,5 +27,6 @@ function solution(board)
     }
     
     answer = Math.pow(maxLen, 2);
+    
     return answer;
 }
