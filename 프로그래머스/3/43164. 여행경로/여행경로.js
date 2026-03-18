18a304d64d29964ca3d034e1bcf2fc748a986846
김민경 <162336698+m01310g@users.noreply.github.com>
2026-03-19 00:34:09 +0900

    [level 3] Title: 여행경로, Time: 9.03 ms, Memory: 36 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -0,0 +1,43 @@
+function solution(tickets) {
+    var answer = [];
+    const graph = new Map();
+    const visited = Array(tickets.length).fill(false);  
+    const path = ["ICN"];
+    
+    for (let i = 0; i < tickets.length; i++) {
+        const [a, b] = tickets[i];
+        
+        if (!graph.has(a)) graph.set(a, []);
+        graph.get(a).push({ to: b, id: i });
+    }
+    
+    for (const [key, value] of graph) {
+        value.sort((a, b) => a.to.localeCompare(b.to));
+    }
+    
+    const dfs = (curr) => {
+        if (path.length === tickets.length + 1) {
+            answer = [...path];
+            return true;
+        }
+        
+        for (const ticket of graph.get(curr) || []) {
+            if (!visited[ticket.id]){
+                const next = ticket.to;
+                visited[ticket.id] = true;
+                path.push(next);
+                
+                if (dfs(next)) return true;
+                
+                path.pop();
+                visited[ticket.id] = false;
+            }
+        }
+        
+        return false;
+    }
+    
+    dfs("ICN");
+    
+    return answer;
+}
