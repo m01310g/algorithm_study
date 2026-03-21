7007a6ba37b1256882be908212b4a213820888cb
김민경 <162336698+m01310g@users.noreply.github.com>
2026-03-21 16:56:30 +0900

    [level 3] Title: 거스름돈, Time: 39.76 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.js
@@ -0,0 +1,14 @@
+function solution(n, money) {
+    var answer = 0;
+    const dp = Array(n + 1).fill(0);
+    dp[0] = 1;
+    
+    for (const coin of money) {
+        for (let i = coin; i <= n; i++) {
+            dp[i] += dp[i - coin];
+            dp[i] %= 1000000007;
+        }
+    }
+        
+    return dp[n];
+}
