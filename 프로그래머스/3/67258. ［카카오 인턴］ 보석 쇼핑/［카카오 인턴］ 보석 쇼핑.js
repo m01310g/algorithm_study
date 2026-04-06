55f6770255a90adc6e41f78f1fe01132b3f6e8ea
김민경 <162336698+m01310g@users.noreply.github.com>
2026-04-06 18:19:34 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 41.73 ms, Memory: 39.9 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -1,9 +1,9 @@
 function solution(gems) {
     var answer = [];
-    let left = 0, right = 0;
     const gemMap = new Map();
     const n = (new Set(gems)).size;
-    let bestLength = 100000;
+    let left = 0, right = 0;
+    let bestLength = Infinity;
     
     while (right < gems.length) {
         gemMap.set(gems[right], (gemMap.get(gems[right]) || 0) + 1);
@@ -14,10 +14,12 @@ function solution(gems) {
             
             if (right - left < bestLength) {
                 bestLength = right - left;
-                answer = [left + 1, right];    
+                answer = [left + 1, right];
             }
             
-            if (gemMap.get(gems[left]) === 0) gemMap.delete(gems[left]);
+            if (gemMap.get(gems[left]) === 0) {
+                gemMap.delete(gems[left]);
+            }
             
             left++;
         }
